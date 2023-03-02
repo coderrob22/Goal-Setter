3f355bec8469e8f871c1faed7351298899923065
coderrob22 <robertknott7@gmail.com>
2023-03-02 16:53:50 -0500

    goal input added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,32 +1,36 @@
-const jwt= require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
-const protect = asyncHandler(async(req, res, next)=>{
-    let token
+const protect = asyncHandler(async (req, res, next) => {
+  let token
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        try {
-            //Get token from the header
-            token= req.header.authorization.split(' ')[1]
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
 
-            //Verify the token
-            const decode = jwt.verify(token, process.env.JWT_SECRET)
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            //Get user form the token
-            req.user = await User.findById(decode.id).select('-password')
-            next()
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password')
 
-        } catch (error) {
-            console.log(error)
-            res.status(401)
-            throw new Error('Not authorized')
-        }
+      next()
+    } catch (error) {
+      console.log(error)
+      res.status(401)
+      throw new Error('Not authorized')
     }
+  }
 
-    if (!token)
+  if (!token) {
     res.status(401)
-    throw new Error('Not authorized, no token!')
+    throw new Error('Not authorized, no token')
+  }
 })
 
-module.exports= {protect}
+module.exports = { protect }
--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,27 +1,38 @@
-//This file is for all the http routes
-
 import axios from 'axios'
 
 const API_URL = '/api/users/'
 
-//REgister user
-const register= async(userData)=>{
-    const response = await axios.post(API_URL, userData)
+// Register user
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData)
 
-    if(response.data){
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
 
-    return response.data
-
+  return response.data
 }
 
-const logout =()=>{
-    localStorage.removeItem('user')
+// Login user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData)
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+
+  return response.data
 }
 
-const authService ={
-    register,
+// Logout user
+const logout = () => {
+  localStorage.removeItem('user')
+}
+
+const authService = {
+  register,
+  logout,
+  login,
 }
 
 export default authService
